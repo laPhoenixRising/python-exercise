92ab5a7753f10b744bb68e8489d7adb3d119e176
laPhoenixRising <56075917+laPhoenixRising@users.noreply.github.com>
2024-09-09 17:43:54 +0200

    Add files via upload

---
+++ esercizi_Python.py
@@ -0,0 +1,11 @@
+# Scrivere un'istruzione che visualizzi il vostro nome
+
+print('Francesca')
+
+# Scrivere un'istruzione che visualizzi questo testo: Python è l'ideale!
+
+print("Python è l'ideale!")
+
+# Scrivere un'istruzione che visualizzi questo testo: Il gatto ha detto "Miao".
+
+print("""Il gatto ha detto "Miao".""")
