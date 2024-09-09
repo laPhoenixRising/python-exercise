477acf4c72605f418773ad80f776d9661b08d9d7
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-09-09 19:26:47 +0200

    eseguiti esercizi su variabili python

---
+++ esercizi_Python_variabili.py
@@ -0,0 +1,75 @@
+# Esercizio 1
+# Dichiarare una variabile "nome" e assegnargli il tuo nome. Mandare a schermo.
+
+nome = 'Francesca'
+print(nome)
+
+# Esercizio 2
+# Dichiarare una variabile "eta" e assegnargli la tua età. Mandare a schermo.
+
+età = 36
+print(età)
+
+# Esercizio 3
+# Dichiarare una variabile "pi" e assegnargli il valore di pi greco (3,14159). Mandare a schermo.
+
+pi = 3,14159
+print(pi)
+
+# Esercizio 4
+# Creare una variabile "lunghezza" e assegnargli un valore, 
+# quindi riassegnare la variabile a 15. Mandare a schermo.
+
+lunghezza = 65
+lunghezza = 15
+print(lunghezza)
+
+# Esercizio 5
+# Creare una variabile "nome_completo" e 
+# assegnargli una stringa contenente il tuo nome e cognome. 
+# Mandare a schermo.
+
+nome_completo = ('Francesca Colombo')
+print(nome_completo)
+
+# Esercizio 6
+# Creare una variabile "eta_futura" e 
+# assegnargli il valore dell'età che avrai tra 10 anni 
+# (utilizzando la variabile età già esistente). Mandare a schermo.
+
+età_futura = (età + 10)
+print(età_futura)
+
+# Esercizio 7
+# Creare delle variabili "nome", "cognome" ed "anno_di_nascita" 
+# ed assegnarle il valore dei valori. Mandare a schermo in un unico print. 
+# Sovrascrivere tutte le variabili e rimandare a schermo una seconda volta.
+
+nome = 'Francesca'
+cognome = 'Colombo'
+anno_di_nascita = 1988
+
+print(nome,
+      cognome,
+      anno_di_nascita)
+
+nome = 'Maurizio'
+cognome = 'De Magnis'
+anno_di_nascita = 1983
+
+print(nome,
+      cognome,
+      anno_di_nascita)
+
+# Esercizio 8
+# Creare una variabile `eta_attuale` e assegnargli il valore dell'età che hai attualmente, 
+# calcolandola in base all'anno corrente. 
+# Mandare a schermo `eta_attuale`
+
+anno_corrente = 2024
+età_attuale = (anno_corrente - anno_di_nascita)
+
+print(età_attuale)
+
+
+
