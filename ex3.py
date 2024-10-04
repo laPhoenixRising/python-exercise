d8882a89a787f2ca720cb38585e97bd9454f910f
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-04 15:06:03 +0200

    Esecuzione esercizio 3 libro

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print('Ora conterò il pollame:')
+
+print('Galline', 25 + 30 / 6)
+print('Galli', 100 - 25 * 3 % 4)
+
+print('Ora conterò le uova:')
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print('3 + 2 < 5 - 7, vero?')
+
+print(3 + 2 < 5 - 7)
+
+print('Quanto fa 3 + 2?', 3 + 2)
+print('Quanto fa 5 - 7?', 5 - 7)
+
+print('Oh, ecco perchè è falso.')
+
+print('Ancora un pò di matematica:')
+
+print('é maggiore?', 5 > -2)
+print('é maggiore o uguale?', 5 >= -2)
+print('è minore o uguale?', 5 <= -2)
