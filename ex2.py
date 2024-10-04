1daf20129f3af1bbf9b5f530268fb42f9b6d957d
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-04 11:55:31 +0200

    Secondo esercizio libro

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# Questo è un commento.
+# Python ignora tutto quello che viene dopo il carattere #.
+
+print("Potrei avere un'istruzione come questa.") # e il commento successivo sarebbe ignorato
+
+# Potete utilizzare un commento anche per disattivare una riga di codice:
+# print('Questa istruzione non sarà eseguita')
+
+print('Questa invece sarà eseguita.')
