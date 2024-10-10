6a30544264f38204792706bc1a379134a32b849e
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-10 21:34:17 +0200

    Esercizio 9 libro

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Ecco qualche altra cosa strana, ricordatevi di scriverla esattamente nello stesso modo.
+
+days = "Lun Mar Mer Gio Ven Sab Dom"
+months = "Gen\nFeb\nMar\nApr\nMag\nGiu\nLug\nAgo"
+
+print("Questi sono i giorni: ", days)
+print("Questi sono i mesi: ", months)
+
+print("""
+Qui sta accadendo qualcosa.
+Con i tre doppi apici.
+Avremo la possibilità di scrivere ciò che vogliamo.
+Anche 4 righe se vogliamo, o 5, o 6.
+""")
