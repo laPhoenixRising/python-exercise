f1909ea42f5fb579cd029c4dd8a7e3b3fa704205
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-10 21:49:27 +0200

    Esercizio 10 libro

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tEcco una tabulazione."
+persian_cat = "Testo su\ndue righe."
+backslash_cat = "Ecco \\ qualche \\ barra."
+
+fat_cat = """
+Farò una lista:
+\t* Cibo per i gatti
+\t* Pesci
+\t* Erba gatta\n\t* Erba
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
