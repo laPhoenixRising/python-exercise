566c024e0f47321a7a8f6d34eb7679c0e64945f5
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-10 22:00:35 +0200

    Esercizio 11 libro

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("Quanti anni hai?", end=' ')
+age = input()
+print("Quanto sei alto?", end=' ')
+height = input()
+print("Quanto pesi?", end=' ')
+weight = input()
+
+print(f"Quindi, hai {age} anni, sei alto {height} e pesi {weight}.")
