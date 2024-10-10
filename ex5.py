ee6ede0379488b067295c3ea4fc3b5cdb944e9d6
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-10 15:18:35 +0200

    Esercizio 5 libro

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # non è una bugia
+my_height = 74 # pollici
+my_weight = 180 # libbre
+my_eyes = 'blu'
+my_teeth = 'bianchi'
+my_hair = 'castani'
+
+print(f"Parliamo di {my_name}.")
+print(f"E' alto {my_height} pollici.")
+print(f"Pesa {my_weight} libbre.")
+print("In effetti non è molto pesante.")
+print(f"Ha gli occhi {my_eyes} e i capelli {my_hair}.")
+print(f"I suoi denti di solito sono {my_teeth}, dipende da quanto caffè ha bevuto.")
+
+# questa riga è complicata, provate a farla esattamente identica
+total = my_age + my_height + my_weight
+print(f"Se sommo {my_age}, {my_height} e {my_weight} ottengo {total}.")
