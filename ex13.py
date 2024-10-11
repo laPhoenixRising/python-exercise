015a133f3c363b2ab2121b77e4828fdb64d4860b
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-11 13:53:40 +0200

    Esercizio 13 libro

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# la sezione Che cosa dovrebbe apparire spiega come eseguire questo codice
+script, first, second, third = argv
+
+print("E' stato chiamato il programma:", script)
+print("La prima variabile è:", first)
+print("La seconda variabile è:", second)
+print("La terza variabile è:", third)
