57cea990f8b670e5b3a84ad0aadb66b6b91feef2
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-12 19:41:00 +0200

    Esercizio 18 libro

---
+++ ex18.py
@@ -0,0 +1,22 @@
+# questa assomiglia ai programmi che usano argv
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# ok. quell'*args in realtà è inutile, possiamo fare cosi
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# questa riceve solo un argomento
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# questa non riceve nessuna argomento
+def print_none():
+    print("Non ho niente.")
+
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("Primo!")
+print_none()
