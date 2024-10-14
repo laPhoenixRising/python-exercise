1efc8a5f106aa6dd357a61eb3560590e8d7ae220
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-14 22:42:27 +0200

    Modifica esercizio scrittura python

--- esercizio_scrittura.py
+++ esercizio_scrittura.py
@@ -1,3 +1,11 @@
-file = open("Poesia", "x")
+file = open("Poesia", "w")
 file.write("Ciao mondo")
+file.close()
+
+file = open("Poesia", "a")
+file.write(" Come stai?")
+file.close()
+
+file = open("Poesia")
+print(file.read())
 file.close()
