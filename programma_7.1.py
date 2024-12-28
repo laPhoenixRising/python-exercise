c8e3b4dcc594ee491cc4ac3efecd8b9f9d384bab
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-12-28 19:54:33 +0100

    Esercizio ciclo while

---
+++ programma_7.1.py
@@ -0,0 +1,6 @@
+conteggio = int(input("Scegli un numero di partenza "))
+print(f"Conto alla rovescia da {conteggio} a 0")
+while conteggio >= 0:
+    print(conteggio)
+    conteggio -= 1
+print("Ho finito")    
